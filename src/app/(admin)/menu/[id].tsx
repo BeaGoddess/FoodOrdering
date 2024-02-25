ada41f54d16b460d937cf6b0c441c01a0bfc1914
Beatriz Silva <beagaara@gmail.com>
2024-02-25 21:01:12 +0000

    Create admin side

---
+++ src/app/(admin)/menu/[id].tsx
@@ -0,0 +1,71 @@
+import { View, Text, Image, StyleSheet, Pressable } from "react-native";
+import { useState } from "react";
+import { Stack, useLocalSearchParams, useRouter } from "expo-router";
+import products from "@assets/data/products";
+import { defaultPizzaImage } from "@/components/ProductListItem";
+import Button from "@components/Button";
+import { useCart } from "@/provider/CartProvider";
+import { PizzaSize } from "@/types";
+
+const sizes: PizzaSize[] = ["S", "M", "L", "XL"];
+
+const ProductDetailsScreen = () => {
+  // Query params from URL (id)
+  const { id } = useLocalSearchParams();
+  const { addItem } = useCart();
+  const router = useRouter();
+
+  // State variables
+  const [selectedSize, setSelectedSize] = useState<PizzaSize>("M");
+
+  // Data from Products (json)
+  const product = products.find((p) => p.id.toString() === id);
+
+  const addToCart = () => {
+    if (!product) {
+      return;
+    }
+
+    addItem(product, selectedSize);
+    router.push('/cart');
+  };
+
+  if (!product) {
+    return <Text> Product not found </Text>;
+  }
+
+  return (
+    <View style={styles.container}>
+      <Stack.Screen options={{ title: product.name }} />
+      <Image
+        source={{ uri: product.image || defaultPizzaImage }}
+        style={styles.image}
+      />
+
+      <Text style={styles.title}>{product.name}</Text>
+      <Text style={styles.price}>{product.price}€</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    flex: 1,
+    padding: 10,
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 1,
+  },
+  title:{
+    fontSize: 20,
+    fontWeight: 'bold'
+  },
+  price: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+});
+
+export default ProductDetailsScreen;
